95de47b8617c045281a5fcf6dbbc7881dc4afa97
Jared Hensley <jahensle85@me.com>
2017-08-26 14:51:20 -0500

    adding comments

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
+    this.state = {
       videos: [],
       selectedVideo: null
    };
@@ -27,21 +27,22 @@ class App extends Component {
 
   videoSearch(term) {
     YTSearch({key: API_KEY, term: term}, videos => {
-      this.setState({ 
+      this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
-  } 
+  }
 
   render() {
 
     const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
     return (
       <div>
+        // videoSearch debounced callback from above
         <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList 
+        <VideoList
           onVideoSelect={(selectedVideo) => {this.setState({selectedVideo})}}
           videos={this.state.videos}/>
       </div>
